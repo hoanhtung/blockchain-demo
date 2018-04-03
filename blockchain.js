5e9291a220328dcd842dfcf7d850c0fb01195c38
hoanhtung <hoanhtung.xy@gmail.com>
2018-04-03 22:16:00 +0700

    Add files via upload

---
+++ blockchain.js
@@ -0,0 +1,89 @@
+const cluster = require('cluster')
+const dgram = require('dgram')
+const express = require('express')
+
+
+class Block {
+  constructor(index, previousHash, timestamp, data, nonce) {
+    this.index = index
+    this.previousHash = previousHash
+    this.timestamp = timestamp
+    this.nonce = nonce
+    this.hash = null
+  }
+}
+
+
+class Server {
+  constructor() {
+    this.blocks = []
+    this.peers = {}
+
+    // Peer discovery server
+    this.peerServer = dgram.createSocket('udp4')
+    this.peerServer.on('listening', this.onPeerServerListening.bind(this))
+    this.peerServer.on('message', this.onPeerMessage.bind(this))
+
+    // RPC server
+    this.httpServer = express()
+    // TODO: API to show know peers
+    this.httpServer.get('/peers', this.showPeers.bind(this))
+    // TODO: API to show current blocks
+    this.httpServer.get('/blocks', this.showBlocks.bind(this))
+  }
+
+  showPeers(req, res) {
+    res.json(this.peers);
+    console.log(JSON.stringify(this.peers));
+  }
+
+  showBlocks() {
+    console.log(JSON.stringify(this.blocks));
+  }
+
+  start() {
+    if (!cluster.isMaster) return
+
+    // Start peer discovery server
+    cluster.fork().on('online', _ => this.peerServer.bind(2346))
+
+    // Start RPC server
+    cluster.fork().on('online', _ => this.httpServer.listen(2345, _ => {
+      console.info(`RPC server started at 2345.`)
+    }))
+  }
+
+  onPeerServerListening() {
+    const address = this.peerServer.address()
+    console.info(
+      `Peer discovery server started at ${address.address}:${address.port}.`
+    )
+
+    const message = Buffer.from('hello')
+    // TODO: broadcast 'hello' message to subnet '172.28.0.0'
+    this.peerServer.setBroadcast(true);
+    this.peerServer.send(message, 2346 , '172.28.0.0');
+
+
+  }
+
+  onPeerMessage(message, remote) {
+    // TODO: Check if the message is 'hello'
+    const bufferCompare = Buffer.from('hello');
+    if (message.toString('utf8') === 'hello') {
+      if (this.peers[`${remote.address}`]) return;
+    // TODO: Add the peer's address to `this.peers`, if it's not there already
+    console.log(`Peer discovered: ${remote.address}:${remote.port}`)
+    this.peers[`${remote.address}`] = remote.port;
+    const reply = Buffer.from('hello')
+    // TODO: Reply to the peer with the same 'hello' message
+    this.peerServer.send(reply, remote.port, remote.address);
+
+  }
+
+  }
+}
+
+
+exports.Block = Block
+exports.Server = Server
